b9f85bdc1c376debf74dd702e314695e93094afa
info@brickal.org <info@brickal.org@da423e35-1b37-0564-ad7b-93536e643665>
2011-10-21 18:28:02 +0000

    git-svn-id: http://brickal.googlecode.com/svn@3 da423e35-1b37-0564-ad7b-93536e643665

--- trunk/index.php
+++ trunk/index.php
@@ -1,11 +1,66 @@
 <?php
 /*
-|---------------------------------------------------------------
-| BRICKAL FRAMEWORK
-|---------------------------------------------------------------
-| More information:	http://www.brickal.org/
-*/
+ |---------------------------------------------------------------
+ | BRICKAL FRAMEWORK
+ |---------------------------------------------------------------
+ | More information:	http://www.brickal.org/
+ */
 
+/*
+ |---------------------------------------------------------------
+ | SYSTEM FOLDER NAME
+ |---------------------------------------------------------------
+ | This variable must contain the name of your "system" folder.
+ | This directory does not need to be web-accessible.
+ |
+ | NO TRAILING SLASH!
+ */
+$system_folder = "system";
 
+/*
+ |---------------------------------------------------------------
+ | APPLICATION FOLDER NAME
+ |---------------------------------------------------------------
+ | This variable must contain the name of your "application"
+ | folder. This directory does not need to be web-accessible.
+ |
+ | NO TRAILING SLASH!
+ */
+$application_folder = "application";
 
-?>
+/*
+ |---------------------------------------------------------------
+ | FILE EXTENSION
+ |---------------------------------------------------------------
+ | This variable must contain the extension of your PHP files.
+ */
+$extension = ".php";
+
+/*
+ |===============================================================
+ | END OF USER CONFIGURABLE SETTINGS
+ |===============================================================
+ */
+
+//Enable all error reporting during development
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+//Set the exact path to our installation folder
+if (strpos($system_folder, '/') === FALSE) {
+	$system_folder = realpath(dirname(__FILE__) . '/' . $system_folder);
+}
+if (strpos($application_folder, '/') === FALSE) {
+	$application_folder = realpath(dirname(__FILE__) . '/' . $application_folder);
+}
+
+//Define some constants
+define('SYSPATH', $system_folder);
+define('APPPATH', $application_folder);
+define('EXT', $extension);
+
+//Include our core
+require (SYSPATH . '/core' . EXT);
+
+//Start the magic...
+Core::execute();
---
+++ trunk/system/core.php
@@ -0,0 +1,9 @@
+<?php
+
+class Core {
+
+	public static function execute() {
+		echo('exectest');
+	}
+
+}
